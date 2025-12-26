93fadb73cc500c16c28766914a39806d761a34d4
Zaxiriddin Xatamov <hatamovz047786@gmail.com>
2025-12-26 19:58:28 +0500

    yangiladim

--- main/models/services.py
+++ main/models/services.py
@@ -1,13 +1,13 @@
 from .base import Base
 from django.db import models
 
-class Service(Base):
+class Service(models.Model):
     name = models.CharField(max_length=255)
     service_price = models.TextField()
 
 
 class ServicesDate(models.Model):
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
 
     def __str__(self):
         return self.date
